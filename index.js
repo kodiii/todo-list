7820a825936b9f32ffd94dd2e90fd7327fda3704
Ricardo <codzign@gmail.com>
2020-09-24 20:05:26 +0200

    set localstorage

--- index.js
+++ index.js
@@ -1,38 +1,71 @@
 const taskInput = document.getElementById('new-task-input')
 const addTaskBtn = document.getElementById('add-task-btn')
+const taskListContainer = document.getElementById('task-list-container')
 const taskList = document.getElementById('task-list')
 
+let count = 0
+
+
 // create task element items
-function createTaskElement() {
+function createTask() {
 
-    taskInput
+    // taskList.innerHTML = ''
+
     const taskItem = document.createElement('li')
-    taskItem.className = 'task-item'
-    taskItem.textContent = taskInput.value
-    taskList.append(taskItem)
-    // console.log(taskItem)
-
+    const btnContainer = document.createElement('div')
+    const completeBtn = document.createElement('button')
     const deleteBtn = document.createElement('button')
-    deleteBtn.className = 'delete-btn'
-    deleteBtn.innerText = 'DELETE'
-    taskItem.append(deleteBtn)
+
+    if (taskInput.value) {
+        window.localStorage.setItem('task' + count++, taskInput.value)
+        // console.log(localStorage)
+
+        for (const key in localStorage) {
+            console.log(localStorage.getItem(key))
+
+            taskItem
+            taskItem.textContent = localStorage.getItem(key)
+            taskItem.className = 'task-item'
+
+            btnContainer
+            btnContainer.className = 'btn-container'
+            taskItem.append(btnContainer)
+
+            completeBtn
+            completeBtn.className = 'complete-btn'
+            completeBtn.innerText = 'COMPLETE'
+            btnContainer.append(completeBtn)
+
+            deleteBtn
+            deleteBtn.className = 'delete-btn'
+            deleteBtn.innerText = 'DELETE'
+            btnContainer.append(deleteBtn)
+
+            taskList.append(taskItem)
+        }
+    } else {
+        alert('Please add a task.')
+    }
+
+    taskInput.value = ''
+
+    // button event listeners to add and remove data tasks
+    completeBtn.addEventListener('click', function () {
+        taskItem.style.textDecoration = 'line-through'
+        completeBtn.style.backgroundColor = '#f77f00'
+    })
 
     deleteBtn.addEventListener('click', function () {
-        // console.log('hello')
-        taskItem.style.textDecoration = 'line-through'
-        //taskList.removeChild(taskItem)
+        taskList.removeChild(taskItem)
     })
 }
+// createTask()
+
 
 // event listener to create item with input text
 addTaskBtn.addEventListener('click', function (e) {
     e.preventDefault()
-    if (taskInput.value) {
-        createTaskElement()
-        taskInput.value = ''
-    } else {
-        alert('Please add a task.')
-    }
+    createTask()
 })
 
 
