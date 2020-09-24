f5674d9ed12b5175bbea659678201c9369b01507
Ricardo Ribeiro <kodii@kodii.eu>
2020-09-24 23:51:54 +0200

    render tasks

--- index.js
+++ index.js
@@ -3,69 +3,121 @@ const addTaskBtn = document.getElementById('add-task-btn')
 const taskListContainer = document.getElementById('task-list-container')
 const taskList = document.getElementById('task-list')
 
+let savedData
+// variable to serialize localstorage keys
 let count = 0
 
+window.onload = function () {
 
-// create task element items
-function createTask() {
+    document.getElementById('task-list').innerHTML = ''
 
-    // taskList.innerHTML = ''
+    if (savedData !== null) {
 
-    const taskItem = document.createElement('li')
-    const btnContainer = document.createElement('div')
-    const completeBtn = document.createElement('button')
-    const deleteBtn = document.createElement('button')
-
-    if (taskInput.value) {
-        window.localStorage.setItem('task' + count++, taskInput.value)
+        //savedData = localStorage.localStorage.getItem(key)
         // console.log(localStorage)
 
-        for (const key in localStorage) {
-            console.log(localStorage.getItem(key))
+        for (let i = 0; i < localStorage.length; i++) {
+            // console.log(localStorage.key(i))
+            let key = localStorage.key(i)
 
-            taskItem
+            const taskItem = document.createElement('li')
             taskItem.textContent = localStorage.getItem(key)
             taskItem.className = 'task-item'
 
-            btnContainer
+            const btnContainer = document.createElement('div')
             btnContainer.className = 'btn-container'
             taskItem.append(btnContainer)
 
-            completeBtn
+            const completeBtn = document.createElement('button')
             completeBtn.className = 'complete-btn'
             completeBtn.innerText = 'COMPLETE'
             btnContainer.append(completeBtn)
 
-            deleteBtn
+            const deleteBtn = document.createElement('button')
             deleteBtn.className = 'delete-btn'
             deleteBtn.innerText = 'DELETE'
             btnContainer.append(deleteBtn)
 
             taskList.append(taskItem)
+            //taskList.style.display = 'none'
+
+            // button event listeners to mark as complete and remove data tasks
+            completeBtn.addEventListener('click', function () {
+                taskItem.style.textDecoration = 'line-through'
+                completeBtn.style.backgroundColor = '#f77f00'
+            })
+
+            deleteBtn.addEventListener('click', function () {
+                localStorage.removeItem(key)
+                taskList.removeChild(taskItem)
+            })
         }
+    }
+}
+
+// create task element items
+function createTask(e) {
+    e.preventDefault()
+
+    taskList.innerHTML = ''
+
+    if (taskInput.value) {
+        savedData = localStorage.setItem('task' + count++, taskInput.value)
+        // console.log(localStorage)
+
+        for (let i = 0; i < localStorage.length; i++) {
+            // console.log(localStorage.key(i))
+            let key = localStorage.key(i)
+
+            const taskItem = document.createElement('li')
+            taskItem.textContent = localStorage.getItem(key)
+            taskItem.className = 'task-item'
+
+            const btnContainer = document.createElement('div')
+            btnContainer.className = 'btn-container'
+            taskItem.append(btnContainer)
+
+            const completeBtn = document.createElement('button')
+            completeBtn.className = 'complete-btn'
+            completeBtn.innerText = 'COMPLETE'
+            btnContainer.append(completeBtn)
+
+            const deleteBtn = document.createElement('button')
+            deleteBtn.className = 'delete-btn'
+            deleteBtn.innerText = 'DELETE'
+            btnContainer.append(deleteBtn)
+
+            taskList.append(taskItem)
+            //taskList.style.display = 'none'
+
+            // button event listeners to mark as complete and remove data tasks
+            completeBtn.addEventListener('click', function () {
+                taskItem.style.textDecoration = 'line-through'
+                completeBtn.style.backgroundColor = '#f77f00'
+            })
+
+            deleteBtn.addEventListener('click', function () {
+                localStorage.removeItem(key)
+                taskList.removeChild(taskItem)
+            })
+        }
+
     } else {
         alert('Please add a task.')
     }
 
+    // clear input field value
     taskInput.value = ''
 
-    // button event listeners to add and remove data tasks
-    completeBtn.addEventListener('click', function () {
-        taskItem.style.textDecoration = 'line-through'
-        completeBtn.style.backgroundColor = '#f77f00'
-    })
-
-    deleteBtn.addEventListener('click', function () {
-        taskList.removeChild(taskItem)
-    })
 }
-// createTask()
 
+function renderTasks() {
+    document.getElementsByClassName('li')
+    document.getElementsByClassName('complete-btn')
+    document.getElementsByClassName('delete-btn')
+}
 
 // event listener to create item with input text
-addTaskBtn.addEventListener('click', function (e) {
-    e.preventDefault()
-    createTask()
-})
+addTaskBtn.addEventListener('click', createTask)
 
 
